b74d76d6c3c85a6c15cbd792e6a7e830463273f3
Apurv Bhavsar <bapurv557@gmail.com>
2018-04-25 19:18:12 -0400

    Update StudentInfo.java

--- src/main/java/com/example/model/StudentInfo.java
+++ src/main/java/com/example/model/StudentInfo.java
@@ -19,62 +19,64 @@ import com.fasterxml.jackson.annotation.JsonFormat.Feature;
 @Entity
 @Table
 public class StudentInfo implements Serializable {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(columnDefinition = "serial",name = "STUDENT_ID")
-	private long studentId;
-	private String firstName;
-	private String lastName;
-	private String dateOfBirth;
-	private String classDetails;
-	@OneToOne(fetch = FetchType.EAGER,cascade = {CascadeType.ALL})
-	@com.fasterxml.jackson.annotation.JsonFormat(with = Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
-	private ScoreCard scoreCard;
-	//@OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
-	//@JoinColumn(name="Scorecard_ID")
-   /// private ScoreCard score;
-	
-	//public ScoreCard getScore() {
-	//	return score;
-	//}
-	//public void setScore(ScoreCard score) {
-	//	this.score = score;
-	//}
-	
-	public long getStudentId() {
-		return studentId;
-	}
-	public ScoreCard getScoreCard() {
-		return scoreCard;
-	}
-	public void setScoreCard(ScoreCard scoreCard) {
-		this.scoreCard = scoreCard;
-	}
-	public void setStudentId(long studentId) {
-		this.studentId = studentId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getDateOfBirth() {
-		return dateOfBirth;
-	}
-	public void setDateOfBirth(String dateOfBirth) {
-		this.dateOfBirth = dateOfBirth;
-	}
-	public String getClassDetails() {
-		return classDetails;
-	}
-	public void setClassDetails(String classDetails) {
-		this.classDetails = classDetails;
-	}
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ @Column(columnDefinition = "serial", name = "STUDENT_ID")
+ private long studentId;
+ private String firstName;
+ private String lastName;
+ private String dateOfBirth;
+ private String classDetails;
+ @OneToOne(fetch = FetchType.EAGER, cascade = {
+  CascadeType.ALL
+ })
+ @com.fasterxml.jackson.annotation.JsonFormat(with = Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+ private ScoreCard scoreCard;
+ //@OneToOne(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+ //@JoinColumn(name="Scorecard_ID")
+ /// private ScoreCard score;
+
+ //public ScoreCard getScore() {
+ //	return score;
+ //}
+ //public void setScore(ScoreCard score) {
+ //	this.score = score;
+ //}
+
+ public long getStudentId() {
+  return studentId;
+ }
+ public ScoreCard getScoreCard() {
+  return scoreCard;
+ }
+ public void setScoreCard(ScoreCard scoreCard) {
+  this.scoreCard = scoreCard;
+ }
+ public void setStudentId(long studentId) {
+  this.studentId = studentId;
+ }
+ public String getFirstName() {
+  return firstName;
+ }
+ public void setFirstName(String firstName) {
+  this.firstName = firstName;
+ }
+ public String getLastName() {
+  return lastName;
+ }
+ public void setLastName(String lastName) {
+  this.lastName = lastName;
+ }
+ public String getDateOfBirth() {
+  return dateOfBirth;
+ }
+ public void setDateOfBirth(String dateOfBirth) {
+  this.dateOfBirth = dateOfBirth;
+ }
+ public String getClassDetails() {
+  return classDetails;
+ }
+ public void setClassDetails(String classDetails) {
+  this.classDetails = classDetails;
+ }
 }
