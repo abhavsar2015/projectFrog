501f4daa4826c9568b6c31f16f26a3eaf7adbe9c
Apurv Bhavsar <bapurv557@gmail.com>
2018-04-25 19:16:41 -0400

    Update ScoreCard.java

--- src/main/java/com/example/model/ScoreCard.java
+++ src/main/java/com/example/model/ScoreCard.java
@@ -16,37 +16,37 @@ import org.hibernate.annotations.GenericGenerator;
 @Entity
 @Table
 public class ScoreCard {
-	@Id
-	@GenericGenerator(name="customUUID", strategy="uuid2")
-	@GeneratedValue(generator="customUUID")
-	private String scoreCardId;
-    private String subject1;
-    private String subject2;
-    private String subject3;
-	public String getScoreCardId() {
-		return scoreCardId;
-	}
-	public void setScoreCardId(String scoreCardId) {
-		this.scoreCardId = scoreCardId;
-	}
-	
-	public String getSubject1() {
-		return subject1;
-	}
-	public void setSubject1(String subject1) {
-		this.subject1 = subject1;
-	}
-	public String getSubject2() {
-		return subject2;
-	}
-	public void setSubject2(String subject2) {
-		this.subject2 = subject2;
-	}
-	public String getSubject3() {
-		return subject3;
-	}
-	public void setSubject3(String subject3) {
-		this.subject3 = subject3;
-	}
-    
+ @Id
+ @GenericGenerator(name = "customUUID", strategy = "uuid2")
+ @GeneratedValue(generator = "customUUID")
+ private String scoreCardId;
+ private String subject1;
+ private String subject2;
+ private String subject3;
+ public String getScoreCardId() {
+  return scoreCardId;
+ }
+ public void setScoreCardId(String scoreCardId) {
+  this.scoreCardId = scoreCardId;
+ }
+
+ public String getSubject1() {
+  return subject1;
+ }
+ public void setSubject1(String subject1) {
+  this.subject1 = subject1;
+ }
+ public String getSubject2() {
+  return subject2;
+ }
+ public void setSubject2(String subject2) {
+  this.subject2 = subject2;
+ }
+ public String getSubject3() {
+  return subject3;
+ }
+ public void setSubject3(String subject3) {
+  this.subject3 = subject3;
+ }
+
 }
